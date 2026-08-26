f04bcc0146af0e9f41ab1afee0a6351f5a365134
Will Darney <willdarney@Desktop-G97643.local>
2026-08-26 09:31:32 -0400

    Parallelize Android RX888 R2IQ chunks

--- source_modules/rx888_source/sddc_core/fft_mt_r2iq.h
+++ source_modules/rx888_source/sddc_core/fft_mt_r2iq.h
@@ -4,10 +4,20 @@
 #include "fftw3.h"
 #include "config.h"
 #include <algorithm>
+#include <atomic>
+#include <condition_variable>
+#include <cstdint>
+#include <mutex>
 #include <string.h>
+#include <thread>
+#include <vector>
 
 // use up to this many threads
+#if defined(__ANDROID__)
+#define N_MAX_R2IQ_THREADS 4
+#else
 #define N_MAX_R2IQ_THREADS 1
+#endif
 #define PRINT_INPUT_RANGE  0
 
 static const int halfFft = FFTN_R_ADC / 2;    // half the size of the first fft at ADC 64Msps real rate (2048)
@@ -92,6 +102,12 @@ private:
     void * r2iqThreadf_avx2(r2iqThreadArg *th);
     void * r2iqThreadf_avx512(r2iqThreadArg *th);
     void * r2iqThreadf_neon(r2iqThreadArg *th);
+#if defined(__ANDROID__)
+    void * r2iqThreadf_android(r2iqThreadArg *th);
+    void androidWorkerLoop(unsigned workerIdx);
+    void androidRunBlock(fftwf_complex* pout, int mfft, int mtunebin, const fftwf_complex* filter, const fftwf_complex* filter2, bool lsb, int decimate);
+    void processFftChunk(r2iqThreadArg* th, const float* adcInTime, int k, int mfft, int mtunebin, const fftwf_complex* filter, const fftwf_complex* filter2, bool lsb, fftwf_complex* pout, int decimate);
+#endif
 
     fftwf_complex **filterHw;       // Hw complex to each decimation ratio
 
@@ -100,6 +116,26 @@ private:
 	fftwf_plan plans_f2t_c2c[NDECIDX];
 
     uint32_t processor_count;
+    uint32_t allocated_thread_count;
+#if defined(__ANDROID__)
+    uint32_t android_worker_count;
+    std::vector<std::thread> android_workers;
+    std::mutex android_work_mutex;
+    std::condition_variable android_work_cv;
+    std::condition_variable android_done_cv;
+    std::atomic<int> android_next_k;
+    uint64_t android_work_seq;
+    uint64_t android_completed_seq;
+    bool android_stop_workers;
+    const float* android_adc_in_time;
+    fftwf_complex* android_pout;
+    int android_mfft;
+    int android_mtunebin;
+    int android_decimate;
+    const fftwf_complex* android_filter;
+    const fftwf_complex* android_filter2;
+    bool android_lsb;
+#endif
     r2iqThreadArg* threadArgs[N_MAX_R2IQ_THREADS];
     std::mutex mutexR2iqControl;                   // r2iq control lock
     std::thread r2iq_thread[N_MAX_R2IQ_THREADS]; // thread pointers
@@ -110,6 +146,14 @@ struct r2iqThreadArg {
 
 	r2iqThreadArg()
 	{
+		ADCinTime = nullptr;
+		ADCinFreq = nullptr;
+		inFreqTmp = nullptr;
+		plan_t2f_r2c = nullptr;
+		for (int d = 0; d < NDECIDX; d++)
+		{
+			plans_f2t_c2c[d] = nullptr;
+		}
 #if PRINT_INPUT_RANGE
 		MinMaxBlockCount = 0;
 		MinValue = 0;
@@ -120,6 +164,8 @@ struct r2iqThreadArg {
 	float *ADCinTime;                // point to each threads input buffers [nftt][n]
 	fftwf_complex *ADCinFreq;         // buffers in frequency
 	fftwf_complex *inFreqTmp;         // tmp decimation output buffers (after tune shift)
+	fftwf_plan plan_t2f_r2c;
+	fftwf_plan plans_f2t_c2c[NDECIDX];
 #if PRINT_INPUT_RANGE
 	int MinMaxBlockCount;
 	int16_t MinValue;
